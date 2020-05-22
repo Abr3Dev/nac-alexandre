d8be41cfd0c8917518bdab16227c009f88f4d744
Raphael Camargo <raphaelcamar@outlook.com>
2020-05-22 12:05:29 -0300

    renomeando projeto

---
+++ Andrerm83092Client/src/br/com/fiap/application/Atualizar.java
@@ -0,0 +1,25 @@
+package br.com.fiap.application;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import br.com.fiap.to.ProdutoTO;
+
+public class Atualizar {
+	private Client client = Client.create();
+	
+	public int putService(ProdutoTO produto) {
+				
+		WebResource resource = client.resource("http://localhost:8080/Andrerm83092/rest/produto/"+produto.getCodigo());
+		
+		ClientResponse response = resource.type("application/json").put(ClientResponse.class, produto);
+		
+		if(response.getStatus() == 200) {
+			return response.getStatus();
+		}else {
+			return response.getStatus();
+		}
+	}	
+
+}
